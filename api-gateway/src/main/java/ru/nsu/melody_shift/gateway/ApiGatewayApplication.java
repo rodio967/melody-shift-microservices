b82eb60c5ae85e3e36aff87502068e017ec706d3
rodion <rdr1218@mail.ru>
2026-04-13 21:45:51 +0700

    feature: add api gateway

---
+++ api-gateway/src/main/java/ru/nsu/melody_shift/gateway/ApiGatewayApplication.java
@@ -0,0 +1,11 @@
+package ru.nsu.melody_shift.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
