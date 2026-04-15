d7208e8ee2f172fa1b3c14669ad40c893913f7b6
rodion <rdr1218@mail.ru>
2026-04-15 18:26:41 +0700

    feature: add YouTubeOAuthService

--- user-service/src/main/java/ru/nsu/melody_shift/user/dto/OAuthTokenResponse.java
+++ user-service/src/main/java/ru/nsu/melody_shift/user/dto/OAuthTokenResponse.java
@@ -1,5 +1,6 @@
 package ru.nsu.melody_shift.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OAuthTokenResponse {
 
     @JsonProperty("access_token")
